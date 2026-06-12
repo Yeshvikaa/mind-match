288577227215a786ddbbf83d19f2e665bd7716b7
Yeshvikaa <yeshuprojects@gmail.com>
2026-06-12 10:48:43 +0530

    Update AdminPage.jsx

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useAuth } from '../context/AuthContext'
 
-const API = 'http://localhost:5000/api'
+const API = 'https://mind-match-s0na.onrender.com/api'
 
 const AdminPage = () => {
   const { authHeaders, addToast } = useAuth()
